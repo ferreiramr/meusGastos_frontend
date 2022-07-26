a7a7f0c747528a7f3db2330674e1003fa352baeb
Gabriel Ferreira <bi194509@gmail.com>
2022-07-26 09:43:34 -0300

    feature/alteracao_de_rotas

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <main>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Login/>}/>
+            <Route path="/" element={<Home/>}/>
             <Route path="/Login" element={<Login/>}/>
             <Route path="/Cadastro" element={<Cadastro/>}/>
             <Route path="/Home" element={<Home/>}/>
